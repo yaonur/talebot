0e1f704944bc3d7c522ed489eb4a7cce9c3457d5
yaonur <ya_onur@hotmail.com>
2021-06-15 17:57:08 +0300

    require added

---
+++ takescreenshot.py
@@ -0,0 +1,53 @@
+import pyautogui
+from pynput.mouse import Listener
+import time
+import keyboard
+
+def takeScreenShot():
+    while True:
+        if keyboard.is_pressed(' '):
+            x,y=pyautogui.position()
+            time.sleep(.5)
+            image = pyautogui.screenshot(region=(x, y, 10, 10))
+            filename = pyautogui.prompt(text='enter file name', title='', default='')
+            print(f'/{filename}X{x}Y{y}.jpg')
+            image.save(f'./{filename}X{x}Y{y}.jpg')
+
+
+
+
+# def on_move(x, y):
+#     print('Pointer moved to {0}'.format(
+#         (x, y)))
+#
+#
+# def on_click(x, y, button, pressed):
+#     time.sleep(.5)
+#     image = pyautogui.screenshot(region=(x, y, 10, 10))
+#     filename = pyautogui.prompt(text='enter file name', title='', default='')
+#     print(f'/{filename}X{x}Y{y}.jpg')
+#     image.save(f'./{filename}X{x}Y{y}.jpg')
+#     return False
+#     #     # Stop listener
+#     #     return False
+#
+#
+# def on_scroll(x, y, dx, dy):
+#     listener.stop()
+#     # time.sleep(.5)
+#     # image = pyautogui.screenshot(region=(x, y, 10, 10))
+#     # filename = pyautogui.prompt(text='enter file name', title='', default='')
+#     # print(f'/{filename}X{x}Y{y}.jpg')
+#     # image.save(f'./{filename}X{x}Y{y}.jpg')
+#     # return False
+#
+#
+# # Collect events until released
+# with Listener(
+#         on_move=on_move,
+#         on_scroll=on_scroll,
+#         on_click=on_click,
+# ) as listener:
+#     listener.join()
+if __name__ == "__main__":
+    takeScreenShot()
